c062b255acb57e3e03560cbca7a0bfc97e97b646
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-26 14:34:19 +0000

    Initial project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wack-A-Penguin
+<p align="center">
+  <img src="demo/app-icon.png " alt="Wack-A-Penguin application icon"
+	  title="Wack-A-Penguin application icon" align="center" width="200" height="200" />
+</p>
+</br>
+Based on the wack-a-mole game.
+
+## Examples within this code
+N/A
+
+
+## Demonstration
+Small demonstration as to what the game does when on device.
+
---
+++ wack-a-penguin/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  wack-a-penguin
+//
+//  Created by Bradley Chesworth on 26/03/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
